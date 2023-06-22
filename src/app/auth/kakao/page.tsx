e7ad3d93f94bbe51d80bf804edf638690d825453
Wang HoEun <hoeunwang@gmail.com>
2023-06-22 16:49:35 +0900

    feat: 로그인 핸들러 callback 함수 내부에 선언

--- src/app/auth/kakao/page.tsx
+++ src/app/auth/kakao/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { LoginHandler } from "../../hooks/LoginHandler";
 
 function Kakao() {
   const code: string = new URL(window.location.href).searchParams.get("code")!;
   useEffect(() => {
-    console.log("code 전달 후 get 요청 보내기");
-    console.log(code);
+    LoginHandler(code);
   }, []);
 
   return <div />;
