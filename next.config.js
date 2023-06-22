338700856843ccc01dfa3d7898400f47f7109171
Wang HoEun <hoeunwang@gmail.com>
2023-06-22 16:58:03 +0900

    feat: proxy 서버 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/auth/kakao",
+        destination: "http://43.201.219.27:8080/auth/kakao"
+      }
+    ];
+  }
+};
 
 module.exports = nextConfig;
