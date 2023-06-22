5ee1dfc8b9f103f499d0133f6b73fb4ff362d511
Wang HoEun <hoeunwang@gmail.com>
2023-06-22 16:28:10 +0900

    style: 사용하지 않는 파일 정리 및 초기 page세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./globals.css";
+import "./styles/globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <h1>메인입니다.</h1>
+    </main>
+  );
+}
+
+export default Home;
