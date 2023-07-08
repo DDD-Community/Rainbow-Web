e41a14e4ca390a062e7b6e023de8d88f2f2c02e0
Wang HoEun <hoeunwang@gmail.com>
2023-07-08 21:36:48 +0900

    fix: api주소 변경 + 로그인 유지 코드 삭제

--- src/api/auth/client.tsx
+++ src/api/auth/client.tsx
@@ -6,7 +6,7 @@ const getSessionKeyLocalStorage = () => {
 };
 
 export const authInstance = axios.create({
-  baseURL: "http://43.201.219.27:8080",
+  baseURL: "http://apis.buybye.kr:8080",
   withCredentials: false,
   headers: {
     Authorization: `${getSessionKeyLocalStorage()}`
