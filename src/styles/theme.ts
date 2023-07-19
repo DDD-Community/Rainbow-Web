45a75807e9554aceab620a6bff3bac46726fc3d4
Wang HoEun <hoeunwang@gmail.com>
2023-07-19 10:21:49 +0900

    feat: 색상 디자인 시스템 생성

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const COLORS = {
+  PRIMARY: {
+    hover: "#f04f1e",
+    default: "#FF5B29",
+    disabled: "#FFDACE",
+    bg: "#FFECE8",
+    bgdisabled: "#FFF7F5"
+  },
+  GRAY: {
+    700: "#27262E",
+    600: "#5E6066",
+    500: "#8C9097",
+    400: "#BEC3CC",
+    300: "#D5D9E0",
+    200: "#E9EAF0",
+    100: "#F3F5F9",
+    50: "#F8F9FC"
+  }
+};
