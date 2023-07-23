cc8c79824ff5da82ffc6eae4c5865443ba52658e
SeungHwa Han <97728416+hangry-bird@users.noreply.github.com>
2023-07-23 15:52:34 +0900

    feat: template-layout 생성 #12 (#13)
    * feat: template-layout 생성 #12
    - DefaultContainer 컴포넌트 생성
    
    * style: min-width 변경
    - 320px -> 343px

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import DefaultContainer from "components/defaultContainer";
 import ReactQueryProvider from "../queries/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <DefaultContainer>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </DefaultContainer>
       </body>
     </html>
   );
