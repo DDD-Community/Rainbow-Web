dbfbc29e79a50e085d98ee7169e70ec64e69bd57
hangry-bird <starcjs7@gmail.com>
2023-07-28 18:30:24 +0900

    feat: tailwind custom color 설정#17

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,21 @@ module.exports = {
       },
       maxWidth: {
         375: "375px"
+      },
+      colors: {
+        "gray-700": "#27262E",
+        "gray-600": "#5E6066",
+        "gray-500": "#8C9097",
+        "gray-400": "#BEC3CC",
+        "gray-300": "#D5D9E0",
+        "gray-200": "#E9EAF0",
+        "gray-100": "#F3F5F9",
+        "gray-50": "#F8F9FC",
+        "primary-default": "#FF5B29",
+        "primary-hover": "#F04F1E",
+        "primary-disabled": "#FFDACE",
+        "primary-bg": "#FFECE8",
+        "primary-bg-disabled": "#FFF7F5"
       }
     }
   },
