3659552e385e33f8305703c2b7b4793237ad1e2c
hangry-bird <starcjs7@gmail.com>
2023-07-29 16:04:57 +0900

    feat: tag component 개발 완료 커밋 #17

---
+++ components/tag/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+/**
+ * 호은님이 tailwind-merge library 적용되어 있는 Branch 작업 및 Merge 후 tailwind-join method 적용 예정
+ * tailwind-merge 적용 전 임시로 concat 적용
+ */
+
+const DEFAULT_COMMON_CLASS = "rounded-[40px] px-[8px] py-[3px] m-10-500 ";
+
+interface TagProps {
+  children: React.ReactNode;
+}
+
+export function PrimaryTag({ children }: TagProps) {
+  return <span className={`${DEFAULT_COMMON_CLASS  }bg-primary-default text-white`}>{children}</span>;
+}
+
+export function SecondaryTag({ children }: TagProps) {
+  return (
+    <span
+      className={
+        `${DEFAULT_COMMON_CLASS 
+        }bg-primary-bg-disabled border border-primary-bg text-primary-default`
+      }
+    >
+      {children}
+    </span>
+  );
+}
+
+export function SubTag({ children }: TagProps) {
+  return (
+    <span className={`${DEFAULT_COMMON_CLASS  }bg-gray-50 border border-gray-200 text-gray-600`}>
+      {children}
+    </span>
+  );
+}
