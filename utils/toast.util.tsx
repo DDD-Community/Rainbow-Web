0c606c4a9667c3d1abf2173c257c7fdbebbc41e3
hangry-bird <starcjs7@gmail.com>
2023-07-29 21:56:04 +0900

    feat: infomation toast component 개발 완료 커밋 #17

--- utils/toast.util.tsx
+++ utils/toast.util.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import { toast } from "react-toastify";
-import { IconWhiteChecked, IconPrimaryChecked } from "assets/images/icons/toast";
+import {
+  IconWhiteChecked,
+  IconPrimaryChecked,
+  IconExclamationMark
+} from "assets/images/icons/toast";
 
 const DEFAULT_COMMON_TOAST_OPTIONS = {
   autoClose: 3000,
@@ -33,6 +37,15 @@ export const showSecondaryToast = (text: string, options: ToastOptions = {}) => 
   toast(text, {
     icon: <Image src={IconPrimaryChecked} alt="primary checked icon" />,
     className: "secondary-toast",
+    ...DEFAULT_COMMON_TOAST_OPTIONS,
+    ...options
+  });
+};
+
+export const showInfomationToast = (text: string, options: ToastOptions = {}) => {
+  toast(text, {
+    icon: <Image src={IconExclamationMark} alt="Exclamation Mark Icon" />,
+    className: "infomation-toast",
     ...DEFAULT_COMMON_TOAST_OPTIONS,
     ...options
   });
