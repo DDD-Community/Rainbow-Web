25b6e66258c771aff9337ad5e90fa635a31a03ff
hangry-bird <starcjs7@gmail.com>
2023-07-29 22:15:37 +0900

    feat: divider component 개발 완료 커밋 #17

---
+++ components/divider/index.tsx
@@ -0,0 +1,7 @@
+export function DividerHorizon() {
+  return <div className="bg-gray-100 w-full h-px" />;
+}
+
+export function DividerVertical() {
+  return <div className="bg-gray-100 w-px h-full" />;
+}
