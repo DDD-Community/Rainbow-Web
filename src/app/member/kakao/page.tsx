7d6e09615f96ecf9ce8135f6457844f19756514c
Wang HoEun <hoeunwang@gmail.com>
2023-08-07 23:44:14 +0900

    style: component코드를 제외한 나머지 온보딩 코드 삭제

--- src/app/member/kakao/page.tsx
+++ src/app/member/kakao/page.tsx
@@ -5,8 +5,6 @@ import { LoginHandler } from "./LoginHandler";
 
 function Login() {
   const code: string = new URL(window.location.href).searchParams.get("code")!;
-  // const [email, setEmail] = useState("");
-  // const [props, setProps] = useRecoilState<Props>(propsState);
 
   useEffect(() => {
     LoginHandler(code);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import React from "react";
-// import Start from "./auth/onboarding/start/page";
-import Onboarding from "./member/onboarding/page";
 
 function Home() {
   return (
     <div className="flex flex-center items-center justify-center h-screen">
-      <Onboarding />
+      <p>바이바이</p>
     </div>
   );
 }
