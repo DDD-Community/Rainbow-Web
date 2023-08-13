2c1a5d30d984552817b30cc0c11ab6f5e052b0ae
hangry-bird <starcjs7@gmail.com>
2023-08-13 18:18:19 +0900

    feat: emoji list 출력 고차함수 적용 - Array.map 함수 적용 - surprised emoji 오타 수정

--- components/emojiBottomSheet/index.tsx
+++ components/emojiBottomSheet/index.tsx
@@ -3,8 +3,16 @@
 import React from "react";
 import Image from "next/image";
 import BottomSheet, { BottomSheetProps } from "@/components/bottomSheet";
-import { IconAngry, IconHappy, IconSad, IconSuprised, IconThinking } from "assets/images/emotion";
+import { IconAngry, IconHappy, IconSad, IconSurprised, IconThinking } from "assets/images/emotion";
 import { EmojiTypes } from "types";
+
+const emojiIcons: { [key in EmojiTypes]: string } = {
+  angry: IconAngry,
+  happy: IconHappy,
+  sad: IconSad,
+  surprised: IconSurprised,
+  thinking: IconThinking
+};
 
 interface EmojiTooltipModalProps extends BottomSheetProps {
   onClickEmoji: (emojiType: EmojiTypes) => void;
@@ -15,39 +23,23 @@ export default function EmojiTooltipModal({
   onDismiss,
   onClickEmoji
 }: EmojiTooltipModalProps) {
+  const emojiKeys = Object.keys(emojiIcons);
   return (
     <BottomSheet open={open} onDismiss={onDismiss}>
       <div className="flex items-center justify-center gap-3 mb-14">
-        <Image
-          src={IconHappy}
-          className="w-14 h-14 cursor-pointer"
-          onClick={() => onClickEmoji("happy")}
-          alt="happy emoji"
-        />
-        <Image
-          src={IconThinking}
-          className="w-14 h-14 cursor-pointer"
-          onClick={() => onClickEmoji("thinking")}
-          alt="thinking emoji"
-        />
-        <Image
-          src={IconSuprised}
-          className="w-14 h-14 cursor-pointer"
-          onClick={() => onClickEmoji("suprised")}
-          alt="suprised emoji"
-        />
-        <Image
-          src={IconSad}
-          className="w-14 h-14 cursor-pointer"
-          onClick={() => onClickEmoji("sad")}
-          alt="sad emoji"
-        />
-        <Image
-          src={IconAngry}
-          className="w-14 h-14 cursor-pointer"
-          onClick={() => onClickEmoji("angry")}
-          alt="angry emoji"
-        />
+        {emojiKeys.map(key => {
+          const emojiName = key as EmojiTypes;
+          const emojiImageSrc = emojiIcons[emojiName as EmojiTypes];
+
+          return (
+            <Image
+              src={emojiImageSrc}
+              className="w-14 h-14 cursor-pointer"
+              onClick={() => onClickEmoji(emojiName)}
+              alt={`${emojiName} emoji`}
+            />
+          );
+        })}
       </div>
     </BottomSheet>
   );
