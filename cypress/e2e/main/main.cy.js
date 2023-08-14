ecb43b8126ec5fc38135a67d33bcfa849fb80756
Wang HoEun <hoeunwang@gmail.com>
2023-08-14 20:18:22 +0900

    fix: cypress코드 주석 처리

--- cypress/e2e/main/main.cy.js
+++ cypress/e2e/main/main.cy.js
@@ -1,13 +1,13 @@
-describe("example to-do app", () => {
-  beforeEach(() => {
-    cy.visit("http://www.buybye.kr/");
-  });
+// describe("example to-do app", () => {
+//   beforeEach(() => {
+//     cy.visit("http://www.buybye.kr/");
+//   });
 
-  it("should contain an element with h2.text-2xl class", () => {
-    cy.get("h2.text-2xl").should("exist");
-  });
+//   it("should contain an element with h2.text-2xl class", () => {
+//     cy.get("h2.text-2xl").should("exist");
+//   });
 
-  it("should contain 4 elements with h2.text-2xl class", () => {
-    cy.get("h2.text-2xl").should("have.length", 4);
-  });
-});
+//   it("should contain 4 elements with h2.text-2xl class", () => {
+//     cy.get("h2.text-2xl").should("have.length", 4);
+//   });
+// });
