d0e3485d7e2c0250cc9f05f95da3731d63bbcf0b
Wang HoEun <hoeunwang@gmail.com>
2023-08-20 00:21:41 +0900

    fix: member_kakao 파일 주석처리

--- src/app/member/kakao/page.tsx
+++ src/app/member/kakao/page.tsx
@@ -1,15 +1,15 @@
-"use client";
+// "use client";
 
-import React, { useEffect } from "react";
-import { LoginHandler } from "./LoginHandler";
+// import React, { useEffect } from "react";
+// import { LoginHandler } from "./LoginHandler";
 
-function Login() {
-  const code: string = new URL(window.location.href).searchParams.get("code")!;
+// function Login() {
+//   const code: string = new URL(window.location.href).searchParams.get("code")!;
 
-  useEffect(() => {
-    LoginHandler(code);
-  }, []);
+//   useEffect(() => {
+//     LoginHandler(code);
+//   }, []);
 
-  return <div />;
-}
-export default Login;
+//   return <div />;
+// }
+// export default Login;
