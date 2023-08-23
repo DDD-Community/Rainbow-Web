2677c152f82565c09e4b97711a31013385c92afc
Wang HoEun <hoeunwang@gmail.com>
2023-08-23 20:57:27 +0900

    fix: salary page 서버사이드에서도 실행되게 코드 추가

--- src/app/member/kakao/page.tsx
+++ src/app/member/kakao/page.tsx
@@ -1,28 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
-import { authInstance } from "@/src/api/auth/client";
-import { LoginDataType } from "@/src/constant/api.constant";
-// import { LoginHandler } from "./LoginHandler";
-
-interface LoginResponseType {
-  data: LoginDataType;
-}
-export const fetchAuth = (code: string): Promise<LoginResponseType> =>
-  authInstance.get(`/members/login?code=${code}`);
+import React, { useEffect } from "react";
+import { LoginHandler } from "./LoginHandler";
 
 function Kakao() {
   const code: string = new URL(window.location.href).searchParams.get("code")!;
 
   useEffect(() => {
-    fetchAuth(code).then((response: LoginResponseType) => {
-      if (response.data.data.email) {
-        window.location.replace("/member/onboarding");
-      }
-      // const JWT = response.data.data.accessToken;
-      // if (JWT) localStorage.setItem("EXIT_LOGIN_TOKEN", JWT);
-      // console.log("로그인 성공");
-    });
+    LoginHandler(code);
   }, []);
+
+  return <div />;
 }
 export default Kakao;
