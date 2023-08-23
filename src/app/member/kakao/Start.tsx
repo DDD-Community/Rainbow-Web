8f9582a5afce742f01023e8d968209f3bfe730a2
Wang HoEun <hoeunwang@gmail.com>
2023-08-23 20:20:05 +0900

    feat: start 페이지 애니메이션 반영

--- src/app/member/kakao/Start.tsx
+++ src/app/member/kakao/Start.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Intro } from "@/public/assets/motions";
 import { KAKAO_URL } from "@/src/constant/api.constant";
+import LottieAnimation from "@/src/hooks/LottieAnimation";
 
 function Start() {
   const loginHandler = () => {
@@ -8,12 +10,19 @@ function Start() {
   };
 
   return (
-    <>
-      <h1>안녕하세요 바이바이입니다</h1>
-      <button type="button" onClick={loginHandler}>
+    <div className="flex flex-col justify-center">
+      <div className="flex flex-col items-start pt-20 pb-10">
+        <div className="sb-25-600 text-gray-700">안녕하세요 바이바이입니다</div>
+      </div>
+      <LottieAnimation animation={Intro} />
+      <button
+        type="button"
+        className="flex justify-center items-center bg-gray-start text-white r-18-600 w-full "
+        onClick={loginHandler}
+      >
         카카오 계정으로 시작하기
       </button>
-    </>
+    </div>
   );
 }
 
