a8f56abe896c54ec574ec086ef14cdb282c0c4b7
Wang HoEun <hoeunwang@gmail.com>
2023-08-30 21:14:56 +0900

    style: start 페이지 css 설정

--- src/app/member/kakao/Start.tsx
+++ src/app/member/kakao/Start.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { IconBuyBye, IconKakao } from "@/public/assets/images/icons";
 import { Intro } from "@/public/assets/motions";
+import { ButtonField } from "@/src/components/Common/Button/ButtonField";
 import { KAKAO_URL } from "@/src/constant/api.constant";
 import LottieAnimation from "@/src/hooks/LottieAnimation";
 
@@ -10,20 +12,26 @@ function Start() {
   };
 
   return (
-    <div className="w-343 flex flex-col justify-center">
-      <div className="flex flex-col items-center pt-20 pb-10">
-        <div className="sb-25-600 text-gray-700">안녕하세요 </div>
-        <div className="sb-25-600 text-gray-700"> 바이바이 입니다</div>
-      </div>
-      <LottieAnimation animation={Intro} />
-      <div className="bg-gray-500 py-5 text-white">
-        <button
-          type="button"
-          className="flex justify-center items-center text-white r-18-600 w-full pb-4 "
-          onClick={loginHandler}
-        >
-          카카오 계정으로 시작하기
-        </button>
+    <div className="w-full flex flex-col justify-center items-center bg-gray-700 h-full">
+      <div className="w-[343px] flex flex-col justify-center">
+        <div className="flex flex-col items-center mt-20 mb-10">
+          <div className="m-16-500 text-gray-400">지출 관리의 시작 </div>
+          <div className="m-16-500 text-gray-400"> 바이바이와 함께해요</div>
+          <IconBuyBye />
+        </div>
+        <LottieAnimation animation={Intro} className="px-[26px] mt-[26px]" />
+        <ButtonField className="mb-[20px]">
+          <button
+            type="button"
+            className="flex justify-center items-center bg-kakao-yellow rounded-[8px] w-full h-[52px]"
+            onClick={loginHandler}
+          >
+            <div className="px-[14px]">
+              <IconKakao />
+            </div>
+            <div className="m-16-500 text-kakao-brown">카카오 계정으로 시작하기</div>
+          </button>
+        </ButtonField>
       </div>
     </div>
   );
