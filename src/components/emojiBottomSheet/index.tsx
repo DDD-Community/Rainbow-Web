5b10bd52ae93e1a8966cd3c25c0d9f005653d87e
hangry-bird <starcjs7@gmail.com>
2023-09-05 00:04:33 +0900

    fix: emoji bottom sheet icon error fix#60

--- src/components/emojiBottomSheet/index.tsx
+++ src/components/emojiBottomSheet/index.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import BottomSheet, { BottomSheetProps } from "@/src/components/bottomSheet";
-import {
-  IconAngry,
-  IconHappy,
-  IconSad,
-  IconSurprised,
-  IconThinking
-} from "@/public/assets/images/emotion";
+
+import IconAngry from "@/public/assets/images/emotion/angry";
+import IconHappy from "@/public/assets/images/emotion/happy";
+import IconSad from "@/public/assets/images/emotion/sad";
+import IconSurprised from "@/public/assets/images/emotion/surprised";
+import IconThinking from "@/public/assets/images/emotion/thinking";
+
 import { EmojiTypes } from "types";
 
-const emojiIcons: { [key in EmojiTypes]: string } = {
-  angry: IconAngry,
-  happy: IconHappy,
-  sad: IconSad,
-  surprised: IconSurprised,
-  thinking: IconThinking
+const emojiIcons: { [key in EmojiTypes]: React.ReactNode } = {
+  angry: <IconAngry />,
+  happy: <IconHappy />,
+  sad: <IconSad />,
+  surprised: <IconSurprised />,
+  thinking: <IconThinking />
 };
 
 interface EmojiTooltipModalProps extends BottomSheetProps {
@@ -35,19 +34,36 @@ export default function EmojiTooltipModal({
       <div className="flex items-center justify-center gap-3 mb-14">
         {emojiKeys.map((key, index) => {
           const emojiName = key as EmojiTypes;
-          const emojiImageSrc = emojiIcons[emojiName as EmojiTypes];
 
           return (
-            <Image
+            <button
+              type="button"
               key={index}
-              src={emojiImageSrc}
-              className="w-14 h-14 cursor-pointer"
+              className="cursor-pointer"
               onClick={() => onClickEmoji(emojiName)}
-              alt={`${emojiName} emoji`}
-            />
+            >
+              {convertEmotionIcon(emojiName)}
+            </button>
           );
         })}
       </div>
     </BottomSheet>
   );
 }
+
+const convertEmotionIcon = (emotionType: EmojiTypes) => {
+  switch (emotionType) {
+    case "angry":
+      return <IconAngry width={56} height={56} />;
+    case "happy":
+      return <IconHappy width={56} height={56} />;
+    case "sad":
+      return <IconSad width={56} height={56} />;
+    case "surprised":
+      return <IconSurprised width={56} height={56} />;
+    case "thinking":
+      return <IconThinking width={56} height={56} />;
+    default:
+      return <IconAngry width={56} height={56} />;
+  }
+};
