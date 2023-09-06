c86fa1a111012414769fc64389161ed92ad0d454
Wang HoEun <hoeunwang@gmail.com>
2023-09-06 12:44:16 +0900

    fix: redirect uri 수정 kakao로

--- src/constant/api.constant.ts
+++ src/constant/api.constant.ts
@@ -1,8 +1,8 @@
 export const CLIENT_ID = "b3ee341ecd11261443a552fa9fbe2d58";
 const isProduction = process.env.NODE_ENV === "production";
 const REDIRECT_URI = isProduction
-  ? "http://www.buybye.kr/member/login" // 배포 모드
-  : "http://localhost:3000/member/login"; // 개발 모드
+  ? "http://www.buybye.kr/member/kakao" // 배포 모드
+  : "http://localhost:3000/member/kakao"; // 개발 모드
 export const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 export const TOKEN = {
