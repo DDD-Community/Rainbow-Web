534a2234f3b2f516c954fb790ae5f5c4fde79cb6
Wang HoEun <hoeunwang@gmail.com>
2023-09-06 21:38:12 +0900

    style: 목표지출 위치 수정

--- src/components/monthlyTarget/index.tsx
+++ src/components/monthlyTarget/index.tsx
@@ -8,8 +8,8 @@ function MonthlyTarget({ target, spentCost }: monthlyTargetProps) {
     <div className="flex flex-col">
       <div className="flex flex-start sb-35-600 text-gray-700">{spentCost} 원</div>
       <div className="flex flex-end">
-        <div className="m-12-500 text-gray-600">목표지출</div>{" "}
-        <div className="m-12-500 text-primary-default">{target}만원</div>
+        <div className="ml-[260px] m-12-500 text-gray-600">목표지출 </div>
+        <div className="ml-[4px] m-12-500 text-primary-default">{target}만원</div>
       </div>
     </div>
   );
