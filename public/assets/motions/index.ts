238863167482af9a548c90af0872adb8f9f58733
Wang HoEun <hoeunwang@gmail.com>
2023-09-06 20:54:41 +0900

    feat: loading화면 설정

--- public/assets/motions/index.ts
+++ public/assets/motions/index.ts
@@ -1,2 +1,3 @@
 export { default as OnboardingMotion } from "@/public/assets/motions/onboarding-motion.json";
 export { default as Intro } from "@/public/assets/motions/intro.json";
+export { default as LoadingSpinner } from "./loading-spinner.json";
