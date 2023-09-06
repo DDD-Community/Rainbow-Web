1706047fd19460f7847a8861dca7758451b6942f
Wang HoEun <hoeunwang@gmail.com>
2023-09-06 11:09:56 +0900

    fix: start 페이지 window 제거 link로 변경

--- src/app/member/kakao/Start.tsx
+++ src/app/member/kakao/Start.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { IconBuyBye, IconKakao } from "@/public/assets/images/icons";
 import { Intro } from "@/public/assets/motions";
 import { ButtonField } from "@/src/components/Common/Button/ButtonField";
@@ -7,10 +8,6 @@ import { KAKAO_URL } from "@/src/constant/api.constant";
 import LottieAnimation from "@/src/hooks/LottieAnimation";
 
 function Start() {
-  const loginHandler = () => {
-    window.location.replace(KAKAO_URL);
-  };
-
   return (
     <div className="w-full flex flex-col justify-center items-center bg-gray-700 h-full">
       <div className="w-[343px] flex flex-col justify-center">
@@ -21,16 +18,15 @@ function Start() {
         </div>
         <LottieAnimation animation={Intro} className="px-[26px] mt-[26px]" />
         <ButtonField className="mb-[20px]">
-          <button
-            type="button"
+          <Link
+            href={KAKAO_URL}
             className="flex justify-center items-center bg-kakao-yellow rounded-[8px] w-full h-[52px]"
-            onClick={loginHandler}
           >
             <div className="px-[14px]">
               <IconKakao />
             </div>
             <div className="m-16-500 text-kakao-brown">카카오 계정으로 시작하기</div>
-          </button>
+          </Link>
         </ButtonField>
       </div>
     </div>
