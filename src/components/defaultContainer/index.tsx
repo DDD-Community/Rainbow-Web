af6cc9cfb314a78031ec34d2c2eafe87517a0a15
hangry-bird <starcjs7@gmail.com>
2023-09-09 01:20:32 +0900

    style: defaultContainer 첫번째 요소 height 설정#77

--- src/components/defaultContainer/index.tsx
+++ src/components/defaultContainer/index.tsx
@@ -7,7 +7,7 @@ interface DefaultContainerProps {
 function DefaultContainer({ children }: DefaultContainerProps) {
   return (
     <div className="shadow-md mx-auto">
-      <div className="flex flex-col w-375 min-w-375 max-w-375 min-h-screen my-0 mx-auto overflow-auto [&>*:first-child]:h-[calc(100vh-53px)] [&>*:first-child]:overflow-auto">
+      <div className="flex flex-col w-375 min-w-375 max-w-375 min-h-screen max-h-screen my-0 mx-auto [&>*:first-child]:h-screen [&>*:first-child]:overflow-auto">
         {children}
       </div>
     </div>
