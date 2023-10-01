727badbf26e32d0d841398285b862c3cec38200b
hangry-bird <starcjs7@gmail.com>
2023-10-01 22:01:11 +0900

    feat: 금액 입력 - 지출내용 표시 #72

--- src/app/plus/cost/page.tsx
+++ src/app/plus/cost/page.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import { useRecoilState } from "recoil";
 import { useRouter } from "next/navigation";
 import NavigationBar from "src/components/navigationBar";
-import { expensePriceState } from "src/recoil/plus.atoms";
+import { expensePriceState, expenseDetailState } from "src/recoil/plus.atoms";
 import Section from "./section";
 import Footer from "./footer";
 
 export default function PlusCostPage() {
   const router = useRouter();
   const [price, setPrice] = useRecoilState(expensePriceState);
+  const [expenseDetail] = useRecoilState(expenseDetailState);
 
   const handlePrice = (value: string) => {
     const replaceValue = value.replace(/[^\d]/g, "");
@@ -31,7 +32,7 @@ export default function PlusCostPage() {
 
       <Section
         price={price}
-        expenditureDetail="바나바나 바나나"
+        expenditureDetail={expenseDetail}
         onChangePrice={e => {
           handlePrice(e.target.value);
         }}
