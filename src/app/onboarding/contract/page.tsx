cd3e744b6e90427ef72284fbe87eb6aaebd96f2a
hangry-bird <starcjs7@gmail.com>
2023-10-17 19:19:32 +0900

    refactor: 약관 페이지 레이아웃 css 조정#89

--- src/app/onboarding/contract/page.tsx
+++ src/app/onboarding/contract/page.tsx
@@ -13,22 +13,22 @@ export default function Contract() {
   const checkboxData = [
     {
       id: 1,
-      label: "개인 정보 취급 방침",
+      label: "개인 정보 취급 방침 (필수)",
       link: "https://www.notion.so/ceaa67fb76344d3eb7aa53f9e4e69815?pvs=4"
     },
     {
       id: 2,
-      label: "사용자 이용 약관",
+      label: "사용자 이용 약관 (필수)",
       link: "https://www.notion.so/7ff6ae8f70144af3b40f2e13e3f68528?pvs=4"
     },
     {
       id: 3,
-      label: "제 3자 정보 제공 동의",
+      label: "제 3자 정보 제공 동의 (필수)",
       link: "https://www.notion.so/3-f986b22d6f5b46f8bcf50b1a06f20fbc?pvs=4"
     },
     {
       id: 4,
-      label: "만 14세 이상입니다",
+      label: "만 14세 이상입니다 (필수)",
       link: "https://www.notion.so/14-759400ed1cac4a2592a75fbc0bb84e71?pvs=4"
     }
   ];
@@ -63,47 +63,66 @@ export default function Contract() {
   const canActiveNextButton = Boolean(!contractAgreed);
 
   return (
-    <div className="w-343 flex flex-col justify-center">
-      <div className="flex flex-col items-start pt-20 pb-10">
-        <div>👀</div>
-        <div className="sb-25-600 text-gray-700">
-          앱 사용을 위해 <br />
-          약관에 동의해주세요
+    <div className="w-full h-full flex flex-col p-4">
+      <div className="flex flex-col items-start h-full min-h-[470px] pt-20">
+        <div className="flex flex-col gap-2">
+          <span className="sb-25-600">🔎</span>
+          <p className="sb-25-600 text-gray-700 leading-[130%]">
+            앱 사용을 위해 <br />
+            약관에 동의해주세요
+          </p>
         </div>
-        <div className="r-12-400 text-gray-600 py-5">
+
+        <p className="r-12-400 text-gray-600 mt-[18px] mb-[28px]">
           정확한 지출 데이터 조회와 쉬운 바이바이 서비스 제공을 위해 약관 동의가 꼭 필요합니다.
-        </div>
-        <div className="bg-gray-50 w-full rounded-[10px] flex items-center p-3 border-[1px] border-gray-50 mb-3">
+        </p>
+
+        <button
+          type="button"
+          className="bg-gray-50 w-full rounded-[10px] flex items-center px-[13px] py-[15px] border border-black[0.03] mb-4"
+          onClick={(e: React.MouseEvent<HTMLElement>) => {
+            toggleAllCheckboxes();
+            e.preventDefault();
+          }}
+        >
           <Checkbox
             size="m"
             labelText="전체 약관에 동의합니다"
             isChecked={Object.values(checkboxStates).every(value => value)}
-            onChange={toggleAllCheckboxes}
+            onChange={() => {}}
           />
-        </div>
-        <div className="flex flex-col w-full">
+        </button>
+
+        <div className="flex flex-col w-full gap-1.5">
           {checkboxData.map(checkbox => (
-            <div className="flex justify-between p-1">
-              <Checkbox
-                size="s"
-                labelText={checkbox.label}
-                isChecked={checkboxStates[checkbox.id]}
-                onChange={() => handleCheckboxChange(checkbox.id)}
-              />
-              <Link href={checkbox.link} className="m-14-500 text-gray-500">
+            <div className="flex justify-between grid grid-cols-[1fr_auto] pr-4 pl-[13px]">
+              <div className="min-w-full">
+                <Checkbox
+                  size="s"
+                  labelText={checkbox.label}
+                  isChecked={checkboxStates[checkbox.id]}
+                  onChange={() => handleCheckboxChange(checkbox.id)}
+                />
+              </div>
+              <a
+                target="_blank"
+                href={checkbox.link}
+                className="w-[25px] shrink-0 m-14-500 text-gray-500" rel="noreferrer"
+              >
                 보기
-              </Link>
+              </a>
             </div>
           ))}
         </div>
-        <ButtonField>
-          <Link href="/onboarding/following" className="w-full">
-            <PrimaryButton color="default" size="large" disabled={canActiveNextButton}>
-              동의하고 가입하기
-            </PrimaryButton>
-          </Link>
-        </ButtonField>
       </div>
+
+      <ButtonField className="py-0">
+        <Link href="/onboarding/following" className="w-full">
+          <PrimaryButton color="default" size="large" disabled={canActiveNextButton}>
+            동의하고 가입하기
+          </PrimaryButton>
+        </Link>
+      </ButtonField>
     </div>
   );
 }
