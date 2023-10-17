d41369cb3f211a387e57f91c24294de629417a8c
hangry-bird <starcjs7@gmail.com>
2023-10-17 16:52:46 +0900

    refactor: 생년월일 페이지 css, 기능 리팩터링#89

--- src/app/onboarding/birth/page.tsx
+++ src/app/onboarding/birth/page.tsx
@@ -1,128 +1,123 @@
-/* eslint-disable no-restricted-globals */
-
+/* eslint-disable */
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { useRecoilState, useRecoilValue } from "recoil";
-import Link from "next/link";
 import { PrimaryButton } from "@/src/components/Common/Button";
 import { ButtonField } from "@/src/components/Common/Button/ButtonField";
-import { TextInput } from "@/src/components/Common/Input";
 import { birthDateState, checkingState, nickNameState } from "@/src/recoil/user.atoms";
 
+const yearRegex = /^[0-9]{4}$/;
+const monthRegex = /^[0-9]{1,2}$/;
+const dateRegex = /^[0-9]{1,2}$/;
+
 export default function Birth() {
-  const [year, setYear] = useState("");
-  const [month, setMonth] = useState("");
-  const [date, setDate] = useState("");
+  const router = useRouter();
+
   const [birth, setBirth] = useRecoilState(birthDateState);
   const nicknameValue = useRecoilValue(nickNameState);
 
+  const [year, setYear] = useState<string>(birth ? birth.split("-")[0] : "");
+  const [month, setMonth] = useState<string>(birth ? birth.split("-")[1] : "");
+  const [date, setDate] = useState<string>(birth ? birth.split("-")[2] : "");
+
   const checkingValue = useRecoilValue(checkingState);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [error, setError] = useState(false);
 
-  const isNumber = (value: any) => {
-    if (/^[0-9]*$/.test(value)) {
-      return true; // 숫자로만 이루어진 경우 true 반환
-    }
-    setErrorMessage("년, 월, 일은 숫자로 입력해주세요.");
-    setError(true);
-    return false; // 숫자로만 이루어져 있지 않으면 false 반환
-  };
+  const isActive = yearRegex.test(year) && monthRegex.test(month) && dateRegex.test(date);
 
-  useEffect(() => {
-    if (birth) {
-      const [savedYear, savedMonth, savedDate] = birth.split("-");
-      setYear(savedYear);
-      setMonth(savedMonth);
-      setDate(savedDate);
-    }
-    if (!isNumber(year) || !isNumber(month) || !isNumber(date)) {
-      // 한글을 입력하거나 숫자가 아닌 경우에만 오류 표시
-      setErrorMessage("년, 월, 일은 숫자로 입력해주세요.");
-      setError(true);
-    } else {
-      setErrorMessage(""); // 숫자 입력이 올바른 경우 오류 메시지를 초기화
-      setError(false);
-    }
-  }, [birth]);
-
-  const handleNext = () => {
+  const handleNextButton = () => {
     const yearNum = parseInt(year, 10);
     const monthNum = parseInt(month, 10);
     const dateNum = parseInt(date, 10);
 
     const birthDate = new Date(yearNum, monthNum - 1, dateNum + 1);
-
     const formattedBirthDate = birthDate.toISOString().split("T")[0];
+
     setBirth(formattedBirthDate);
+
+    router.push(`/onboarding/${checkingValue ? "checking" : "salary"}`);
   };
 
-  const getInputValue = (label: string) => {
-    if (label === "년") return year;
-    if (label === "월") return month;
-    if (label === "일") return date;
-    return "";
-  };
-
-  const setInputValue = (label: string, value: string) => {
-    if (label === "년") setYear(value);
-    else if (label === "월") setMonth(value);
-    else if (label === "일") setDate(value);
-  };
-
-  const canActiveNextButton = Boolean(!year || !month || !date || error);
+  const handleChangeYear = (e: React.ChangeEvent<HTMLInputElement>) => setYear(e.target.value);
+  const handleChangeMonth = (e: React.ChangeEvent<HTMLInputElement>) => setMonth(e.target.value);
+  const handleChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => setDate(e.target.value);
 
   return (
-    <div className="w-343 flex flex-col justify-center">
-      <div className="flex flex-col items-start pt-20 pb-10">
-        <div>🥳</div>
-        <div className="sb-25-600 text-gray-700">
-          {nicknameValue}님의 <br />
-          생일을 입력해주세요
+    <div className="w-full h-full flex flex-col p-4">
+      <div className="h-full min-h-[300px] pt-20">
+        <div className="flex flex-col gap-[44px]">
+          <div className="flex flex-col gap-2">
+            <span className="sb-25-600">🥳</span>
+            <p className="sb-25-600 text-gray-700 leading-[130%]">
+              {nicknameValue}님의 <br />
+              생일을 입력해주세요
+            </p>
+          </div>
+
+          <div className="flex gap-2.5">
+            <div className="relative w-full h-[52px]">
+              <input
+                type="text"
+                id="year"
+                value={year}
+                onChange={handleChangeYear}
+                className="w-full h-full px-4 py-[15px] border border-gray-200 rounded-[10px] r-16-400 text-gray-700"
+                maxLength={4}
+                placeholder="1995"
+              />
+              <label
+                htmlFor="year"
+                className="absolute right-[16px] top-1/2 translate-y-[-50%] m-16-500 text-primary-default"
+              >
+                년
+              </label>
+            </div>
+
+            <div className="relative w-full h-[52px]">
+              <input
+                type="text"
+                id="month"
+                value={month}
+                onChange={handleChangeMonth}
+                className="w-full h-full px-4 py-[15px] border border-gray-200 rounded-[10px] r-16-400 text-gray-700"
+                maxLength={2}
+                placeholder="2"
+              />
+              <label
+                htmlFor="month"
+                className="absolute right-[16px] top-1/2 translate-y-[-50%] m-16-500 text-primary-default"
+              >
+                월
+              </label>
+            </div>
+
+            <div className="relative w-full h-[52px]">
+              <input
+                type="text"
+                id="date"
+                value={date}
+                onChange={handleChangeDate}
+                className="w-full h-full px-4 py-[15px] border border-gray-200 rounded-[10px] r-16-400 text-gray-700"
+                maxLength={2}
+                placeholder="20"
+              />
+              <label
+                htmlFor="date"
+                className="absolute right-[16px] top-1/2 translate-y-[-50%] m-16-500 text-primary-default"
+              >
+                일
+              </label>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex gap-4">
-        {["년", "월", "일"].map(label => (
-          <TextInput key={label}>
-            <TextInput.Border>
-              <TextInput.Content
-                value={getInputValue(label)}
-                onChange={e => setInputValue(label, e.target.value)}
-              />
-              <div className="text-primary-default">{label}</div>
-            </TextInput.Border>
-          </TextInput>
-        ))}
-      </div>
-      {errorMessage && <div className="m-12-500 text-primary-default ">{errorMessage}</div>}
-      <div>
-        <ButtonField>
-          {checkingValue ? (
-            <Link href="/onboarding/checking" className="w-full flex justify-end">
-              <PrimaryButton
-                color="default"
-                size="small"
-                disabled={canActiveNextButton}
-                onClick={handleNext}
-              >
-                확인
-              </PrimaryButton>
-            </Link>
-          ) : (
-            <Link href="/onboarding/salary" className="w-full flex justify-end">
-              <PrimaryButton
-                color="default"
-                size="small"
-                disabled={canActiveNextButton}
-                onClick={handleNext}
-              >
-                확인
-              </PrimaryButton>
-            </Link>
-          )}
-        </ButtonField>
-      </div>
+
+      <ButtonField className="py-0">
+        <PrimaryButton color="default" size="small" disabled={!isActive} onClick={handleNextButton}>
+          확인
+        </PrimaryButton>
+      </ButtonField>
     </div>
   );
 }
